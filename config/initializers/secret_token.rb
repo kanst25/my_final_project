f40f601fa50e8428322207c2c5c77b908ea4f78c
Kavir Naik <knaik2014@kellogg.northwestern.edu>
2013-10-23 15:11:48 -0500

    Final Project Setup
    Setup the new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyFinalProject::Application.config.secret_key_base = 'd11bb9804aec79ff70a85ba3766e2334b9b77b7a632e3ee5b3e693693a486a978b5b1b7df63f00ebc7a2e3f48f9f38af442eef0bb79ed4865663deb100b0a843'
